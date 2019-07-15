f705ea88fa8bab55ebfcf9d9877d2888aae3518b
Emmanuel <clarke_ley@live.fr>
2019-07-15 14:15:50 +0200

    Fix migration issue

--- src/Migrations/Version20190715120710.php
+++ src/Migrations/Version20190715120710.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190714182906 extends AbstractMigration
+final class Version20190715120710 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,9 @@ final class Version20190714182906 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE picture ADD album_id INT DEFAULT NULL, ADD image_size INT NOT NULL, ADD updated_at DATETIME NOT NULL, CHANGE pic image_name VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE album (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, date DATE NOT NULL, caption VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, album_id INT DEFAULT NULL, date DATE NOT NULL, caption VARCHAR(255) NOT NULL, image_name VARCHAR(255) NOT NULL, image_size INT NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_16DB4F891137ABCF (album_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F891137ABCF FOREIGN KEY (album_id) REFERENCES album (id)');
-        $this->addSql('CREATE INDEX IDX_16DB4F891137ABCF ON picture (album_id)');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +33,7 @@ final class Version20190714182906 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F891137ABCF');
-        $this->addSql('DROP INDEX IDX_16DB4F891137ABCF ON picture');
-        $this->addSql('ALTER TABLE picture DROP album_id, DROP image_size, DROP updated_at, CHANGE image_name pic VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('DROP TABLE album');
+        $this->addSql('DROP TABLE picture');
     }
 }
