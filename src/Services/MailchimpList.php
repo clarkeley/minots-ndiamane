9fd4ef47e048a5e23a4dc902aaaf6cb2f3565ab7
Emmanuel <clarke_ley@live.fr>
2019-10-04 11:47:33 +0200

    'newsletter-feature' 100%, ending

--- src/Services/MailchimpList.php
+++ src/Services/MailchimpList.php
@@ -13,9 +13,6 @@ class MailchimpList
         $builder = $form->getData();
         $mailchimp = new MailChimp('ed0ab95c3efa7ca550d2b7ad7a75c61b-us4');
 
-        //dd($result = $MailChimp->get('lists'), print_r($result));
-
-
         $list_id = 'e39b70bcac';
 
         $result = $mailchimp->post("lists/$list_id/members", [
