53fb2fc82933d5cddbc938a05fce2236e30b8c0b
Emmanuel <clarke_ley@live.fr>
2019-11-18 13:56:09 +0100

    fix migration 169

--- src/Migrations/Version20191013175129.php
+++ src/Migrations/Version20191013175129.php
@@ -22,7 +22,6 @@ final class Version20191013175129 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE news_letter');
         $this->addSql('ALTER TABLE event CHANGE event event LONGTEXT NOT NULL');
     }
 
@@ -31,7 +30,6 @@ final class Version20191013175129 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE news_letter (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE event CHANGE event event VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
